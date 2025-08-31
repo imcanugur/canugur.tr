6b42e0c6a25d4152f117e03f37b5d0e0d2ba7b5f
Uğur CAN <ugurc1477@gmail.com>
2025-08-31 18:34:20 +0300

    fixed.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { GoogleTagManager } from "@next/third-parties/google";
-import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
